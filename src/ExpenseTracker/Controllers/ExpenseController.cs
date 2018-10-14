817dc4d817446cd7e977a8d920b3b4ce53f64239
Stoyan Ruzmanov <stoyan.ruzmanov@gmail.com>
2018-10-14 15:12:00 +0300

    Initial

---
+++ src/ExpenseTracker/Controllers/ExpenseController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExpenseTracker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private static ExpenseListViewModel expenseList = new ExpenseListViewModel();
+
+        public ExpenseController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View(expenseList);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string expenseName)
+        {
+            expenseList.Expenses.Add(expenseName);
+
+            return View(expenseList);
+        }
+    }
+}
---
+++ src/ExpenseTracker/Models/ExpenseListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ExpenseTracker.Models
+{
+    public class ExpenseListViewModel
+    {
+        public ExpenseListViewModel()
+        {
+            this.Expenses = new List<string>();
+        }
+
+        public ICollection<string> Expenses { get; set; }
+    }
+}
