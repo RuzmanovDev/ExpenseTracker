426a4b4dc970d446be6f75a0f9a7606ec12eeb53
Ruzmanov <stoyan.ruzmanov@gmail.com>
2019-01-03 23:19:29 +0200

    fixed build

--- src/ExpenseTracker.WebApi/Controllers/Authentication/UsersController.cs
+++ src/ExpenseTracker.WebApi/Controllers/Authentication/UsersController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using ExpenseTracker.WebApi.DtoModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
