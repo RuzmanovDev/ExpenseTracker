1ea27b74d75d75ee37e9c24a430e8133792fd5e2
Ruzmanov <stoyan.ruzmanov@gmail.com>
2019-01-04 22:34:36 +0200

    vue exploring

--- client/expense-tracker-vue/src/App.vue
+++ client/expense-tracker-vue/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ExpenseList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ExpenseList from './expense/ExpenseList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ExpenseList
   }
 }
 </script>
